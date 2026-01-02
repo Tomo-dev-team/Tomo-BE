0e4a21f9f037375a6e137a4c212bd9522597d244
myeongsung kim <134512691+tomchaccom@users.noreply.github.com>
2026-01-02 23:14:08 +0900

    refactor : 프론트 파일 정리 및 백엔드 폴더 정리 (#5)
    Co-authored-by: tomchccom <dreamkms2014@gmail.com>

--- frontend/app/src/main/java/com/markoala/tomoandroid/utils/GenerateInviteCode.kt
+++
@@ -1,9 +0,0 @@
-package com.markoala.tomoandroid.utils
-
-fun generateInviteCode(userId: String): String {
-    return if (userId.isNotBlank() && userId.length >= 4) {
-        "TOMO-${userId.takeLast(4)}"
-    } else {
-        "TOMO-0000"
-    }
-}
